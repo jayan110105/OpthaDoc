53a060d4f99719c819f4cae90320138a8fa45544
jayan110105 <jayan.mitmpl2022@learner.manipal.edu>
2024-08-27 14:56:12 +0530

    User 2 layout chnages

--- lib/ui/screens/User2.dart
+++ lib/ui/screens/User2.dart
@@ -10,85 +10,158 @@ class User2 extends StatelessWidget {
         title: Text("User 2"),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Distance Vision'),
-                SizedBox(height: 20,),
-                Text('Pinhole'),
-              ],
-            ),
-            Column(
-              children: [
+                SizedBox(width: 90),
                 Text(
                   "R (Right Eye)",
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Container(
-                        width: 80,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'R',
-                          ),
-                        ),
-                      )
-                ),
-                SizedBox(height: 20,),
-                Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Container(
-                      width: 80,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'R',
-                        ),
-                      ),
-                    )
-                ),
-              ],
-            ),
-            Column(
-              children: [
                 Text(
                   "L (Left Eye)",
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child:
-                      Container(
-                        width: 80,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'L ',
-                          ),
-                        ),
-                      )
+              ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Distance Vision'),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'R',
+                    ),
                   ),
-                SizedBox(height: 20,),
-                Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child:
-                    Container(
-                      width: 80,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'L ',
-                        ),
-                      ),
-                    )
+                ),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'L',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Pinhole'),
+                SizedBox(width: 0,),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'R',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'L',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Glasses'),
+                SizedBox(width: 0,),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'R',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'L',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                Text(
+                    "ARK",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Sp.'),
+                SizedBox(width: 30,),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'R',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'L',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Cyl.'),
+                SizedBox(width: 30,),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'R',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'L',
+                    ),
+                  ),
                 ),
               ],
             ),
